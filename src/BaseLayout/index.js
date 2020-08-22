b52f2f7d2b899234b36aa1a186800a61b7381dd0
Rock Enwerem <rock@cashboxng.com>
2020-08-22 17:15:53 +0100

    add navigation header

--- src/BaseLayout/index.js
+++ src/BaseLayout/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../Components/Header";
 import "../css/style.css";
 
 const PageLayout = (props) => {
   return (
     <>
-      <header>header</header>
+      <Header />
       <aside>sidebar</aside>
       <main>{props.children}</main>
     </>
